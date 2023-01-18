8b14104d527f62de0cf6f4cff9b4471e884a6ae0
Júlio César Assis Dias <juliocesarassisdias14@gmail.com>
2023-01-18 17:59:20 -0300

    adicionando projeto CRUD_produtos

---
+++ CRUD_produtos/js/script_produtos.js
@@ -0,0 +1,19 @@
+class Produto{
+
+    constructor(){
+        this.id = 0;
+        this.nomeProduto = '';
+        this.valor = 0;
+    }
+
+    adicionar(){
+        alert('Cliquei no adicionar!');
+    }
+
+    excluir(){
+        alert('Cliquei no Excluir');
+    }
+
+}
+
+var produto = new Produto();
